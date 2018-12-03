3fcbae95b69395121c19f0c7858680f184c9fb9f
wu allen <cooolallen1@wudeMacBook-Pro.local>
2018-12-03 16:22:40 -0500

    changing the nums_time into an arguement of the run_battle

--- battle/experiement.sh
+++ battle/experiement.sh
@@ -1,8 +1,8 @@
-agent1="ignore"
-agent2="ignore"
-agent3="ignore"
-agent4="ignore"
+agent1="ignore::null"
+agent2="test::agents.SimpleAgent"
+agent3="ignore::null"
+agent4="test::agents.SimpleAgent"
 nums_times=$1
 # --record_json_dir=./battle
 
-python battle/run_battle.py --agents=$agent1::null,$agent2::null,$agent3::null,$agent4::null --num_times=$nums_times --config=PommeTeamCompetition-v0
+python battle/run_battle.py --agents=$agent1,$agent2,$agent3,$agent4 --num_times=$nums_times --config=PommeTeamCompetition-v0
