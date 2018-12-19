df84943c5ce6db7395f9a808d8827f910812ac91
wu allen <cooolallen1@wudeMacBook-Pro.local>
2018-12-18 19:59:14 -0500

    experiment script update

--- battle/experiement.sh
+++ battle/experiement.sh
@@ -3,6 +3,13 @@ agent1="${2}::null"
 agent2="${3}::null"
 agent3="${4}::null"
 nums_times=$5
-# --record_json_dir=./battle
+iter_num=$6
 
-python battle/run_battle.py --agents=$agent0,$agent1,$agent2,$agent3 --num_times=$nums_times --config=PommeTeamCompetition-v0
+dir_name="output_${1}_${2}"
+
+mkdir -p ${dir_name}
+
+for i in $(seq 1 $iter_num)
+do
+    python battle/run_battle.py --agents=$agent0,$agent1,$agent2,$agent3 --num_times=$nums_times --config=PommeTeamCompetition-v0 &> ${dir_name}/output_${i}.txt &
+done
